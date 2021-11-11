a94ac01a65fea0bae840a26e777ece081c7fef08
Richard Blythman <richardblythman@gmail.com>
2021-11-11 20:24:26 +0000

    add first notebook

---
+++ notebooks/_init_paths.py
@@ -0,0 +1,14 @@
+import os
+import os.path as osp
+import sys
+
+def add_path(path):
+    if path not in sys.path:
+        sys.path.insert(0, path)
+
+os.chdir('..')
+
+this_dir = osp.dirname(__file__)
+
+lib_path = osp.join(this_dir, '..')
+add_path(lib_path)
