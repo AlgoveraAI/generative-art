6b7eedbebb689aa4c85d44d07f223b5d2fb5533d
Richard Blythman <richardblythman@gmail.com>
2021-12-28 12:07:34 +0000

    comment out torch imports

--- 4-dcgan-c2d.py
+++ 4-dcgan-c2d.py
@@ -14,15 +14,15 @@ import pickle
 import sys
 import time
 
-import torch
-import torch.nn as nn
-import torch.nn.parallel
-import torch.backends.cudnn as cudnn
-import torch.optim as optim
-import torch.utils.data
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
-import torchvision.utils as vutils
+# import torch
+# import torch.nn as nn
+# import torch.nn.parallel
+# import torch.backends.cudnn as cudnn
+# import torch.optim as optim
+# import torch.utils.data
+# import torchvision.datasets as dset
+# import torchvision.transforms as transforms
+# import torchvision.utils as vutils
 
 def get_input(local=False):
     if local:
